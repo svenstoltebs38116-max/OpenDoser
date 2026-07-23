eb0e46c825bbf8ccf82ce0cbe6c997cbb4365560
sven <svenstoltebs38116@t-online.de>
2026-07-23 21:39:41 +0200

    Add entity state diagnostics

--- entity_manager.py
+++ entity_manager.py
@@ -2,10 +2,14 @@
 
 from __future__ import annotations
 
+import logging
+
 from homeassistant.core import HomeAssistant
 
 from .registry import RoleRegistry
 from .roles import Role
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class EntityManager:
@@ -47,6 +51,19 @@ class EntityManager:
         entity_id = self.get_entity(role)
 
         if entity_id is None:
+            _LOGGER.warning(
+                "Role %s has no assigned entity",
+                role.value,
+            )
             return None
 
-        return self.hass.states.get(entity_id)
+        state = self.hass.states.get(entity_id)
+
+        _LOGGER.warning(
+            "Role=%s Entity=%s State=%s",
+            role.value,
+            entity_id,
+            state,
+        )
+
+        return state
